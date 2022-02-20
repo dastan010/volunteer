f0f93e47ae1dc39e4ff062c2cb980dda259f1977
dastan010 <dastan.mvp97@gmail.com>
2022-02-20 23:24:26 +0600

    changed

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -4,8 +4,8 @@ import { Link, Head } from '@inertiajs/inertia-react';
 export default function Welcome(props) {
     return (
         <>
-            <Head title="Welcome" />
-            <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <Head title="Volunteers" />
+            <div className="back relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="fixed top-0 right-0 px-6 py-4 sm:block">
                     {props.auth.user ? (
                         <>
@@ -15,16 +15,25 @@ export default function Welcome(props) {
                         </>
                     ) : (
                         <>
-                            <Link href={route('login')} className="text-sm text-gray-700 underline">
+                            <Link href={route('login')} className="text-sm underline" style={{
+                                color: 'white'
+                            }}>
                                 Log in
                             </Link>
 
-                            <Link href={route('register')} className="ml-4 text-sm text-gray-700 underline">
+                            <Link href={route('register')} className="ml-4 text-sm text-gray-700 underline" style={{
+                                color: 'white'
+                            }}>
                                 Register
                             </Link>
                         </>
                     )}
                 </div>
+                <h1 style={{
+                    color: 'white'
+                }}>
+                    Volunteers 2022
+                </h1>
             </div>
         </>
     );
